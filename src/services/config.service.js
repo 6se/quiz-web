b1784b7612206fee22525ff97e89867e69c17ef8
x <>
2021-03-30 10:27:29 +0100

    Change backend domain to hosted version

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,8 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    // service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quiz-api-6se.qaalabs.com/';
 
     return service;
 }]);
